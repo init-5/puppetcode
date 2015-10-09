3eed38c9201fee64aedb3e32869ba21c2c8f67b3
unknown <fhlbejl@EJLMXL2071SQ3.atl.fhlb.com>
2015-10-09 12:19:19 -0400

    Added init.pp

---
+++ Puppet/init.pp
@@ -0,0 +1,15 @@
+# Class: services
+#
+# This module manages services
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+class services {
+  include services::application
+}
